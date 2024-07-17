9dea6d089f801576dcca0ec1c273d37f12a042a6
Jovanne Olalo <jovanneolalo@gmail.com>
2024-07-17 20:14:55 +0800

    Add robot and metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";  
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Roboto } from "next/font/google"
+import Head from "next/head"
 
 const roboto = Roboto({
   weight: '400',
@@ -14,7 +15,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return  <>  
-      {/* <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />  */}
+    <Head>
+    <script type="application/ld+json">
+      {JSON.stringify({
+        "@context": "http://schema.org",
+        "@type": "Article",
+        "headline": "Jovanne Olalo",
+        "datePublished": "2024-07-01",
+        "author": {
+          "@type": "Person",
+          "name": "Jovanne Olalo"
+        }, 
+      })}
+    </script>
+  </Head>
       <html lang="en">
         <body className={`${roboto.className}`}>{children}</body>
       </html>
